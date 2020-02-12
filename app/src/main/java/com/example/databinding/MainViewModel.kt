991fc81ea038b1ed6f38b329c4f35cc00e7999cb
Romavip66 <romavip66@gmail.com>
2020-02-12 17:42:30 +0600

    1st commit

---
+++ app/src/main/java/com/example/databinding/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.databinding
+
+import androidx.databinding.Bindable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    val currentRandomFruitName: LiveData<String>
+            get() = FakeRepo.currentRandomFruitName
+
+    fun onChangeRandomFruitClick() = FakeRepo.changeCurrentRandomFruitName()
+
+    @Bindable
+    val editTextContent = MutableLiveData<String>()
+
+    private val _displayedEditTextContent = MutableLiveData<String>()
+    val displayedEditTextContent: LiveData<String>
+            get() = _displayedEditTextContent
+
+    fun onDisplayEditTextContentClick(){
+        _displayedEditTextContent.value = editTextContent.value
+    }
+
+    fun onSelectRandomEditTextFruit(){
+        editTextContent.value = FakeRepo.getRandomFruitName()
+    }
+}
